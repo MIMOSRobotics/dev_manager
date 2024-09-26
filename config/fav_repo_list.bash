bb7267fc87e865032c3ae0c692c94df57a814c97
Mahir Sehmi <mnmahir@gmail.com>
2024-09-26 11:11:03 +0800

    Update fav_repo_list.bash

--- config/fav_repo_list.bash
+++ config/fav_repo_list.bash
@@ -1,4 +1,5 @@
 LIST_FAV_REPO=(
     # "https://github.com/MIMOSRobotics/UGV-Angsa.git $ROS_DISTRO"      # TODO: Modify this repo to support this development manager
     "https://github.com/MIMOSRobotics/UGV-HunterSE.git $ROS_DISTRO"
+    "https://github.com/MIMOSRobotics/UGV-BadakBroga.git $ROS_DISTRO"
 )
